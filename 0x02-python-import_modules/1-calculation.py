d76f57cc9392ea25fbbd76fbd1957cdbb3793209
KaroDev3 <karodev3@gmail.com>
2020-04-30 19:00:16 +0000

    fix task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,3 +6,6 @@ print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
 print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
 print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
