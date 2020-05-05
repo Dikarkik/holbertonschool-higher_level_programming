f88d836fc323fcaee7d6c3d0780b5a4fd93c569e
KaroDev3 <karodev3@gmail.com>
2020-05-05 20:26:34 +0000

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# function that prints a matrix of integers
+
+
+def print_matrix_integer(matrix=[[]]):
+    for line in matrix:
+        for i in range(len(line)):
+            print("{:d}".format(line[i]), end="")
+            if (i != len(line) - 1):
+                print(end=" ")
+        print()
