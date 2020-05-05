eb1e44b541b2cf4926fcebe869638fffa0b2cbb9
KaroDev3 <karodev3@gmail.com>
2020-05-05 23:54:51 +0000

    task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# function that adds 2 tuples
+# Returns a tuple with 2 integers
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    new_tuple = ()
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    new_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1],
+    return new_tuple
