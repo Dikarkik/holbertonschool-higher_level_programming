f79395f5489ac2213bc6d10c5ffa94d9ef868ce8
KaroDev3 <karodev3@gmail.com>
2020-05-06 00:01:11 +0000

    task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# function that returns a tuple with the length
+# of a string and its first character
+
+
+def multiple_returns(sentence):
+    tuple = len(sentence), sentence[0],
+    return (tuple)
