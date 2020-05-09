0cfe5a2c855ccfa77c8a9ecf223761e77877b353
KaroDev3 <karodev3@gmail.com>
2020-05-09 22:44:23 +0000

    task 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import calculator_1 as cal
+import sys
+
+
+def calculator():
+    operators = {'+': cal.add, '-': cal.sub, '*': cal.mul, '/': cal.div}
+
+    if len(sys.argv) > 4 or len(sys.argv) < 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    for key, func in operators.items():
+        if sys.argv[2] == key:
+            a = int(sys.argv[1])
+            b = int(sys.argv[3])
+            print("{:d} {} {:d} = {:d}".format(a, key, b, func(a, b)))
+            exit(0)
+
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
+
+if __name__ == "__main__":
+    calculator()
