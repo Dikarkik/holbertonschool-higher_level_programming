7b9d0cc25bc1391c449233f058a0f8758ffd01e3
KaroDev3 <karodev3@gmail.com>
2020-05-25 01:19:05 +0000

    Add tests in task 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,18 +7,29 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
 
-    def test_void(self):
-        """if no argument"""
+    def test_list_is_empty(self):
         self.assertIsNone(max_integer())
         self.assertIsNone(max_integer([]))
 
-    def test_max(self):
-        """if valid argument, return max integer of the list"""
+    def test_max_at_the_end(self):
         self.assertEqual(max_integer([1, 2, 3]), 3)
+
+    def test_max_at_the_beginning(self):
+        self.assertEqual(max_integer([3, 2, 1]), 3)
+
+    def test_max_in_the_middle(self):
+        self.assertEqual(max_integer([1, 3, 2]), 3)
+
+    def test_one_negative_number_in_the_list(self):
         self.assertEqual(max_integer([-1, 2, 3]), 3)
 
+    def test_only_negative_numbers_in_the_list(self):
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
+
+    def test_list_one_element(self):
+        self.assertEqual(max_integer([1]), 1)
+
     def test_raises(self):
-        """If the argument is not a list"""
         self.assertRaises(TypeError, max_integer, 10)
         self.assertRaises(TypeError, max_integer, 10.0)
 
