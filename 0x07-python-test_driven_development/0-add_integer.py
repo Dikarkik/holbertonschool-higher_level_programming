f91195f303d5b791cfeac71a4494736850d585fc
KaroDev3 <karodev3@gmail.com>
2020-05-25 02:53:22 +0000

    task 0 - Adjust NaN case

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -21,9 +21,5 @@ def add_integer(a, b=98):
         raise OverflowError("a too large")
     if b+1 == b:
         raise OverflowError("b too large")
-    if a != a:
-        raise TypeError("a must be an integer")
-    if b != b:
-        raise TypeError("b must be an integer")
 
     return int(a) + int(b)
