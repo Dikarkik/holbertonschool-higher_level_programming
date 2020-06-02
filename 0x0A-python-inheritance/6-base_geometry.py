09310383d28d314b35ab5d5393f6a3352c6c78ae
KaroDev3 <karodev3@gmail.com>
2020-06-02 18:46:30 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""6. Improve Geometry"""
+
+
+class BaseGeometry:
+    """class based on 5-base_geometry.py"""
+
+    def area(self):
+        """Public instance method: that raises an Exception"""
+        raise Exception("area() is not implemented")
