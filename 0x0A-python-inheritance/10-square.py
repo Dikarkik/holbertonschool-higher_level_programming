110ff882b17993079e1ca9ba51c0193e6ead9312
KaroDev3 <karodev3@gmail.com>
2020-06-04 16:06:13 +0000

    fix task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -15,10 +15,9 @@ class Square(Rectangle):
         """
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         """returns the area of the rectangle"""
         return self.__size * self.__size
 
-    def __str__(self):
-        return "[Rectangle] " + str(self.__size) + "/" + str(self.__size)
