a28fe2a1d684c7b4015a00afc74792a231c2323a
Diana Carolina Quintero <karodev3@gmail.com>
2020-07-13 17:54:14 +0000

    Fix task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,7 +2,7 @@
 -- creates a table 'second_table' and add multiples rows.
 CREATE TABLE IF NOT EXISTS second_table
 (id INT,
-name VARCHAR(256)
+name VARCHAR(256),
 score INT);
 
 INSERT INTO second_table VALUES(1, "John", 10);
