1433d8c9d56d0b239bc80326787ec1bf92227715
Diana Carolina Quintero <karodev3@gmail.com>
2020-08-07 22:04:09 +0000

    0x0F - Add task 4: Cities by states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+4. Cities by states
+script that lists all 'cities' from the database 'hbtn_0e_4_usa'
+
+usage ./4-cities_by_state.py <mysql username> <mysql passwd> <database name>
+example: ./4-cities_by_state.py vagrant pass hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    """ lists all 'cities' from the database """
+
+    db = MySQLdb.connect(host='localhost',
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+     LEFT JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;")
+    rows = cur.fetchall()
+
+    for elem in rows:
+        print(elem)
+
+    cur.close()
