077df6b772abfe3c51c8d047e7ef44aa3a903d26
Diana Carolina Quintero <karodev3@gmail.com>
2020-08-07 18:54:27 +0000

    project 0x0F - Add task 1: Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+1. Filter states
+script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+
+usage ./0-select_states.py <mysql username> <mysql passwd> <database name>
+example: ./0-select_states.py vagrant pass hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+
+def states_starting_N():
+    """  lists all states with a name starting with N """
+
+    if len(sys.argv) != 4:
+        return
+
+    db = MySQLdb.connect(host='localhost',
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%';")
+    for elem in cur:
+        print(cur.fetchone())
+
+if __name__ == "__main__":
+    states_starting_N()
