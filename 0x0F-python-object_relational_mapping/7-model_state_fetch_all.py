03dfe74e66ac089a1ccd120bfb10124ad52eb467
Diana Carolina Quintero <karodev3@gmail.com>
2020-08-08 21:56:22 +0000

    0x0F - Add task 7: All states via SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" 7. All states via SQLAlchemy
+script that lists all State objects from the database hbtn_0e_6_usa
+./7-model_state_fetch_all.py vagrant pass hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import Base, State
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    states = session.query(State).order_by(State.id)
+
+    for instance in states:
+        print("{}: {}".format(instance.id, instance.name))
