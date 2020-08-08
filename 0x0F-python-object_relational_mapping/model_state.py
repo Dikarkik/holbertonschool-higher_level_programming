b1ad44f205e1f16eb411d61992d403443aa948f6
Diana Carolina Quintero <karodev3@gmail.com>
2020-08-08 20:30:50 +0000

    0x0F - Fix task 6: First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,6 +7,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """ class State """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
