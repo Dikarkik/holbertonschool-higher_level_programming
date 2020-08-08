9c28d7d4926af5a58ddaa01953f7a1a87236f071
Diana Carolina Quintero <karodev3@gmail.com>
2020-08-08 16:16:22 +0000

    0x0F - Add task 5: All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -19,13 +19,21 @@ if __name__ == "__main__":
                          db=sys.argv[3])
     cur = db.cursor()
 
-    cur.execute("SELECT cities.name FROM cities\
-     LEFT JOIN states ON cities.state_id=states.id WHERE states.name=(%s)", (sys.argv[4],))
+    cur.execute("""SELECT cities.name
+                    FROM cities
+                    LEFT JOIN states
+                    ON cities.state_id=states.id
+                    WHERE states.name=(%s)""",
+                    (sys.argv[4],))
 
     rows = cur.fetchall()
+    cities = []
 
-    for elem in rows:
-        print(str(elem))
+    if rows:
+        for elem in rows:
+            cities.append(elem[0])
 
+    print(", ".join(cities))
 
     cur.close()
+    db.close()
