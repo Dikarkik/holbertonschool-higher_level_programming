d5757130c88d91e606b09435b7ae8b74d59e5912
Diana Carolina Quintero <karodev3@gmail.com>
2020-08-09 17:20:28 +0000

    0x0F - Add task 14: Cities in state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
 
+
 class City(Base):
     """ Defines City """
     __tablename__ = 'cities'
