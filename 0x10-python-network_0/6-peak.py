c3ff2dcf0fb1c6defc9c8a2a57cd8841f92e841f
Diana Carolina Quintero <karodev3@gmail.com>
2020-08-18 22:47:11 +0000

    Update task 6: Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,24 +8,5 @@ def find_peak(list_of_integers):
     """ finds a peak """
     if len(list_of_integers) == 0:
         return None
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
-
-    peak = list_of_integers[0]
-    i = 0
-    while i < len(list_of_integers):
-        """ peak on extremes """
-        if i == 0:
-            if list_of_integers[i] > list_of_integers[i + 1]:
-                return list_of_integers[i]
-        if i == len(list_of_integers) - 1:
-            if list_of_integers[i] > list_of_integers[i - 1]:
-                return list_of_integers[i]
-
-        """ some peak """
-        if list_of_integers[i] > list_of_integers[i - 1]:
-            if list_of_integers[i] > list_of_integers[i + 1]:
-                return list_of_integers[0]
-
-        i += 1
-    return peak
+    list_of_integers.sort()
+    return list_of_integers[len(list_of_integers) - 1]
