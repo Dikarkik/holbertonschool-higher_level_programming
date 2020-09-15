b5d78f93a3f65dd977e675a8993717190d98b694
KaroDev <karodev3@gmail.com>
2020-09-15 23:00:42 +0000

    Fix task 10: Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,17 +2,16 @@
 
 const number = parseInt(process.argv[2]);
 
-if (isNaN(number)) {
-	console.log(1);
-} else if (number <= 1) {
-	console.log(1);
-} else {
+function fac (num) {
+  if (num === 1) { return (num); }
+  const next = fac(num - 1);
+  return (num * next);
+}
 
-	function fac(num) {
-		if (num === 1)
-			return (num);
-		let next = fac(num - 1);
-		return (num * next);
-	}
-	console.log(fac(number));
+if (isNaN(number)) {
+  console.log(1);
+} else if (number <= 1) {
+  console.log(1);
+} else {
+  console.log(fac(number));
 }
