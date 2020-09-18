dc479dd72fd2a65d98a5d68f5dbf7c3c37fdce6f
KaroDev <karodev3@gmail.com>
2020-09-18 03:18:41 +0000

    Add advanced tasks for javascript project

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// function that increments and calls a function.
+// The function must be visible from outside.
+
+exports.addMeMaybe = function (number, theFunction) {
+  theFunction(number + 1);
+};
