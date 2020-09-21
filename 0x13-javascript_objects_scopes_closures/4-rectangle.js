17bb5c3805931c663343eca2e14ff19282ccf745
KaroDev <karodev3@gmail.com>
2020-09-21 20:26:31 +0000

    Add tasks from 5 to 9 about node

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,27 +1,27 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-	constructor(w, h) {
-		if (Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
-			this.width = w;
-			this.height = h;
-		}
-	}
+  constructor (w, h) {
+    if (Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-	print() {
-		for (let i = 0; i < this.height; i++) {
-			console.log('X'.repeat(this.width));
-		}
-	}
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
 
-	rotate() {
-		let auxiliar = this.width;
-		this.width = this.height;
-		this.height = auxiliar;
-	}
+  rotate () {
+    const auxiliar = this.width;
+    this.width = this.height;
+    this.height = auxiliar;
+  }
 
-	double() {
-		this.width *= 2;
-		this.height *= 2;
-	}
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 };
---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  reversed = [];
+  for (i = list.length - 1; i >= 0; i--) { reversed.push(list[i]); }
+  return reversed;
+};
