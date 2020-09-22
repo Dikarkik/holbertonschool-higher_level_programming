c3b44a72dc3de5a230aee5288fe258f1f14373e3
KaroDev <karodev3@gmail.com>
2020-09-22 18:51:18 +0000

    Fix task 4: Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,14 +8,16 @@ const request = require('request');
 const url = process.argv[2];
 const characterId = 18;
 
-request(url, function (error, response, body) {
-  if (error) { return (console.log(error)); }
+if (url) {
+	request(url, function (error, response, body) {
+		if (error) { return (console.log(error)); }
 
-  const movies = JSON.parse(body).results;
-  let count = 0;
+		const movies = JSON.parse(body).results;
+		let count = 0;
 
-  for (const movie of movies) {
-    if (movie.characters.includes('https://swapi-api.hbtn.io/api/people/' + characterId + '/')) { count++; }
-  }
-  console.log(count);
-});
+		for (const movie of movies) {
+			if (movie.characters.includes('https://swapi-api.hbtn.io/api/people/' + characterId + '/')) { count++; }
+		}
+		console.log(count);
+	});
+}
