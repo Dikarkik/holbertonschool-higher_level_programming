c167da751aa827a6eb252e16cda95a555d2fa9da
KaroDev <karodev3@gmail.com>
2020-09-22 00:10:12 +0000

    Fix task 13: Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,44 +1,21 @@
 #!/usr/bin/node
 
-fs = require('fs')
+const fs = require('fs');
 
-function concatFiles(fileDestiny, file) {
-	fs.readFile(file, 'utf-8', function (err, data) {
-		if (err) {
-			return console.log(err);
-		}
-		fs.appendFile(fileDestiny, data, function (err, result) {
-			if (err) console.log('error', err)
-		})
-	})
+function concatFiles (fileDestiny, file) {
+  fs.readFile(file, 'utf-8', function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    fs.appendFile(fileDestiny, data, function (err, result) {
+      if (err) console.log('error', err);
+    });
+  });
 }
 
-let fileA = process.argv[2]
-let fileB = process.argv[3]
-let fileDestiny = process.argv[4]
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileDestiny = process.argv[4];
 
-concatFiles(fileDestiny, fileA)
-concatFiles(fileDestiny, fileB)
-
-
-
-
-
-// let dataa = fs.readFile(process.argv[2], 'utf-8', function (err, data) {
-// 	if (err) {
-// 		return console.log(err);
-// 	}
-// 	return data
-// })
-
-// console.log(dataa)
-
-// fs.readFile(process.argv[3], 'utf-8', function (err, data) {
-// 	if (err) {
-// 		return console.log(err);
-// 	}
-// 	text += data
-
-// })
-
-// console.log(text)
+concatFiles(fileDestiny, fileA);
+concatFiles(fileDestiny, fileB);
